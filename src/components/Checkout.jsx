33b3d086756d800ae5368f4052673767e6e866dd
Theasasin7 <91683562+Theasasin7@users.noreply.github.com>
2022-04-07 15:34:10 -0400

    arreglando el total en el checkout al crear la orden

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -15,7 +15,7 @@ export const Checkout = () => {
         const order = {
             buyer: values,
             items: cart,
-            total: totalCart(),
+            total: totalCart().tofixed(2),
             date: Timestamp.fromDate(new Date())
         }
 
