b2a39f13d55150bcaa57ca911d8967eeec9209c4
Theasasin7 <ulises.cruz7@hotmail.com>
2022-04-07 16:36:39 -0400

    Updates

--- src/components/ItemList.jsx
+++
@@ -1,9 +0,0 @@
-import { Item } from './Item'
-
-export const ItemList = ({products}) => {
-    return (
-        <div className="grid grid-rows-3 grid-cols-5">
-        {products.map( (ele)=> <Item key={ele.id} {...ele} />)}
-        </div>
-    )
-}
--- src/helper/Button.jsx
+++
@@ -1,5 +0,0 @@
-export const Button = ( {children, click} ) => {
-    return (
-        <button className="border-2 border-gray-900 bg-blue-400 font-semibold mx-10 w-60 rounded-full" onClick={click}>{children}</button>
-    )
-}
